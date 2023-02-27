94a04d09fc68e3ecd19175ce98eba66203aa17ef
Justin Donaldson <jdonaldson@gmail.com>
2023-02-27 12:51:58 -0800

    hooking in OpenAI api

--- src/llmfyi/main.py
+++ src/llmfyi/main.py
@@ -1,17 +1,25 @@
 # This program defines a simple CLI app.
-# It defines a single function "main"
-# It imports sys
-# It prints "hello world" plus the text from the first sys argument if it exists
+# It imports sys, llmfyi.dotfile, and openai.
+# It defines a single function "main".
+# Inside the main function, it does the following:
+# * It calls the dotfile.dotfile method, saving it to a variable named prompt.
+# * It then adds a line of "====\n" to the prompt variable.
+# * It then adds the first sys argument to the result, along with "\n".
+# * It then call the openai.Completion.create function, using text-davinci-003 as the engine, and the prompt variable as the prompt argument.
+# * It prints the text field from the first entry of the choices field returned as a result.
+# It invokes the main function if its __name__ is "__main__".
 
 import sys
+import llmfyi.dotfile
+import openai
 
 def main():
-    if len(sys.argv) > 1:
-        print("Hello world " + sys.argv[1])
-    else:
-        print("Hello world")
+    prompt = llmfyi.dotfile.dotfile()
+    prompt += "====\n"
+    prompt += sys.argv[1] + "\n"
+    choices = openai.Completion.create(engine="text-davinci-003", prompt=prompt)
+    print(choices["choices"][0]["text"])
 
 if __name__ == "__main__":
     main()
-
 
